f86aefc64424280c1642595f61ff61fae829fd18
Gismo2001 <rankenpunk@googlemail.com>
2025-11-24 07:27:18 +0100

    note

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,11 @@
-export default {
+import { defineConfig } from 'vite';
+import { qrcode } from 'vite-plugin-qrcode'; // Geändert zu benanntem Import
+
+export default defineConfig({
   build: {
     sourcemap: true,
-  }
-}
+  },
+  plugins: [
+    qrcode(), // Funktionsaufruf bleibt gleich
+  ],
+});
